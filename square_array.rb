62683790f6737a7fabb2359b1b96cef7cddbc280
Chris Baum <christopher.baum@valpo.edu>
2021-01-12 00:09:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,10 @@ array = [1,2,3]
 def square_array(array)
   array.each do |i|
     i = i*i
-    puts "#{i}"
+    #puts "#{i}"
+    new_array = Array.new
+    new_array = i
+    puts "#{new_array}"
   end
 end
 square_array(array)
